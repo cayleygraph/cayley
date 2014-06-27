95c8ce4fb28cd20dc61a2a735a152251fbcff350
Jonathan Barronville <jonathan@scrapum.photos>
2014-06-27 10:01:47 -0400

    Removed unnecessary activate.sh file.

--- activate.sh
+++
@@ -1,9 +0,0 @@
-# Absolute path this script is in. /home/user/bin
-cd "`dirname '${BASH_SOURCE:-$0}'`"
-SCRIPTPATH="`pwd`"
-echo $dir
-cd - > /dev/null
-
-export GOPATH=$SCRIPTPATH
-#export GOOS="linux"
-#export GOARCH="amd64"
