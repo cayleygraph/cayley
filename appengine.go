592ea816871d256c27d89a1d656667373ecca656
kortschak <dan.kortschak@adelaide.edu.au>
2014-07-23 10:02:11 +0930

    Use build constraints for appengine

--- appengine.go
+++ appengine.go
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cayleyappengine
+// +build appengine
+
+package main
 
 import (
 	"github.com/barakmich/glog"
