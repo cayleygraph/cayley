890f1b69f45c088ef78594dcf044cd7ebbdd5749
Barak Michener <me@barakmich.com>
2015-04-26 17:23:29 -0400

    unique iterator implementation

---
+++ graph/iterator/unique_iterator_test.go
@@ -0,0 +1,31 @@
+package iterator
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestUniqueIteratorBasics(t *testing.T) {
+	allIt := NewFixed(Identity)
+	allIt.Add(1)
+	allIt.Add(2)
+	allIt.Add(3)
+	allIt.Add(3)
+	allIt.Add(2)
+
+	u := NewUnique(allIt)
+
+	expect := []int{1, 2, 3}
+	for i := 0; i < 2; i++ {
+		if got := iterated(u); !reflect.DeepEqual(got, expect) {
+			t.Errorf("Failed to iterate Unique correctly on repeat %d: got:%v expected:%v", i, got, expect)
+		}
+		u.Reset()
+	}
+
+	for _, v := range []int{1, 2, 3} {
+		if !u.Contains(v) {
+			t.Errorf("Failed to find a correct value in the unique iterator.")
+		}
+	}
+}
