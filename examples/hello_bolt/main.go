347978d0e445efd6146f477c9a290c5fc53a65cd
Oren <orengolan@gmail.com>
2016-07-22 23:43:32 -0700

    Example of BoltDB

---
+++ examples/hello_bolt/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/cayleygraph/cayley"
+	"github.com/cayleygraph/cayley/graph"
+	_ "github.com/cayleygraph/cayley/graph/bolt"
+	"github.com/cayleygraph/cayley/quad"
+)
+
+func main() {
+	// path to your new BoltDB
+	path := "./db"
+
+	// Initialize the database
+	graph.InitQuadStore("bolt", path, nil)
+
+	// Open and use the database
+	store, err := cayley.NewGraph("bolt", path, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	store.AddQuad(quad.Make("phrase of the day", "is of course", "Hello BoltDB!", "demo graph"))
+
+	// Now we create the path, to get to our data
+	p := cayley.StartPath(store, quad.String("phrase of the day")).Out(quad.String("is of course"))
+
+	// Now we get an iterator for the path (and optimize it, the second return is if it was optimized,
+	// but we don't care for now)
+	it, _ := p.BuildIterator().Optimize()
+	// remember to cleanup after yourself
+	defer it.Close()
+
+	// Now for each time we can go to next iterator
+	nxt := graph.AsNexter(it)
+	// remember to cleanup after yourself
+	defer nxt.Close()
+
+	// While we have items
+	for nxt.Next() {
+		token := it.Result()                // get a ref to a node
+		value := store.NameOf(token)        // get the value in the node
+		nativeValue := quad.NativeOf(value) // this converts nquad values to normal Go type
+
+		fmt.Println(nativeValue) // print it!
+	}
+	if err := nxt.Err(); err != nil {
+		log.Fatalln(err)
+	}
+}
