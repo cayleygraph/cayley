b4e83a824398c005c3dccb6291b2cb09c47901ce
Denys Smirnov <denis.smirnov.91@gmail.com>
2016-08-24 14:28:07 +0300

    etcd3 iterator

---
+++ graph/etcd/iterator.go
@@ -0,0 +1,105 @@
+package etcd
+
+import (
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+	"golang.org/x/net/context"
+)
+
+func NewIterator(etc *clientv3.Client, pref string, rev int64, opts ...clientv3.OpOption) *Iterator {
+	if pref == "" {
+		panic("prefix is empty")
+	}
+	return &Iterator{
+		etc: etc, pref: pref, rev: rev, opts: opts,
+	}
+}
+
+type Iterator struct {
+	etc  *clientv3.Client
+	pref string
+	rev  int64
+	opts []clientv3.OpOption
+
+	resp   *clientv3.GetResponse
+	offset int
+	err    error
+	result *mvccpb.KeyValue
+
+	requests int
+}
+
+func (it *Iterator) Result() *mvccpb.KeyValue { return it.result }
+func (it *Iterator) Err() error               { return it.err }
+func (it *Iterator) Reset() {
+	it.result = nil
+	it.err = nil
+	it.resp = nil
+	it.offset = 0
+}
+func (it *Iterator) Clone() *Iterator {
+	return NewIterator(it.etc, it.pref, it.rev, it.opts...)
+}
+func getNext(key []byte, pref bool) string {
+	end := make([]byte, len(key))
+	copy(end, key)
+	for i := len(end) - 1; i >= 0; i-- {
+		if end[i] < 0xff {
+			end[i] = end[i] + 1
+			if pref {
+				end = end[:i+1]
+			}
+			return string(end)
+		}
+	}
+	// next prefix does not exist (e.g., 0xffff);
+	// default to WithFromKey policy
+	return "\x00"
+}
+func (it *Iterator) Next() bool {
+	if it.err != nil {
+		return false
+	}
+	if it.resp != nil && it.offset+1 < len(it.resp.Kvs) {
+		it.offset++
+		it.result = it.resp.Kvs[it.offset]
+		return true
+	}
+	ctx := context.TODO()
+	var (
+		opts []clientv3.OpOption
+		key  string
+	)
+	if it.resp == nil {
+		// first iteration
+		opts = append(opts,
+			clientv3.WithPrefix(),
+			clientv3.WithSort(clientv3.SortByKey, clientv3.SortAscend),
+		)
+		key = it.pref
+	} else if !it.resp.More {
+		return false
+	} else {
+		key = getNext(it.resp.Kvs[len(it.resp.Kvs)-1].Key, false)
+		opts = append(opts, clientv3.WithRange(getNext([]byte(it.pref), true)))
+	}
+	if it.rev > 0 {
+		opts = append(opts, clientv3.WithRev(it.rev))
+	}
+	if len(it.opts) > 0 {
+		opts = append(opts, it.opts...)
+	}
+	it.requests++
+	resp, err := it.etc.Get(ctx, key, opts...)
+	if err != nil {
+		it.err = err
+		return false
+	}
+	it.resp = resp
+	if it.rev <= 0 { // fix revision
+		it.rev = it.resp.Header.Revision
+	}
+	it.offset = 0
+	it.result = it.resp.Kvs[it.offset]
+	return true
+}
