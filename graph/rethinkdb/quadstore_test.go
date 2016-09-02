094cd3429a09c730cc35e056b64c245fde6aced7
Mikael Cabot <mikaelcabot@gmail.com>
2016-09-02 11:26:01 +0200

    docker config

--- graph/rethinkdb/quadstore_test.go
+++ graph/rethinkdb/quadstore_test.go
@@ -4,39 +4,20 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cayleygraph/cayley/clog"
 	"github.com/cayleygraph/cayley/graph"
 	"github.com/cayleygraph/cayley/graph/graphtest"
 	"github.com/cayleygraph/cayley/internal/dock"
-	docker "github.com/fsouza/go-dockerclient"
 )
 
 func makeRethinkDB(t testing.TB) (graph.QuadStore, graph.Options, func()) {
-	clog.SetV(5)
-
 	var conf dock.Config
 
 	conf.Image = "rethinkdb:latest"
 	conf.OpenStdin = true
 	conf.Tty = true
 
-	// TODO: Figure out why we cannot connect on docker addr:Port - without having to bind to host.
-	conf.ExposedPorts = map[docker.Port]struct{}{
-		"28015/tcp": {},
-	}
-	conf.PortBindings = map[docker.Port][]docker.PortBinding{
-		"28015/tcp": []docker.PortBinding{
-			{
-				HostPort: "28015",
-			},
-		},
-	}
-
 	addr, closer := dock.Run(t, conf)
-	//addr += ":28015"
-	addr = "localhost:28015" // TODO: Use docker addr (see TODO above)
-
-	t.Logf("Connecting to RethinkDB at: %s", addr)
+	addr += ":28015"
 
 	// Retry connections, docker container might not be ready
 	var err error
