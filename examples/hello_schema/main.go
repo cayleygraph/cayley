e14c3232eda2c7a2f6873edf081cf38b80342650
Denys Smirnov <denis.smirnov.91@gmail.com>
2016-09-18 00:13:05 +0300

    schema: update example

--- examples/hello_schema/main.go
+++ examples/hello_schema/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
 
 	"github.com/cayleygraph/cayley"
@@ -25,6 +26,13 @@ type Person struct {
 	Age     int      `quad:"ex:age"`  // or in a quad tag
 }
 
+type Coords struct {
+	// Object may be without id - it will be generated automatically.
+	// It's also not necessary to have a type definition.
+	Lat float64 `json:"ex:lat"`
+	Lng float64 `json:"ex:lng"`
+}
+
 type quadWriter struct {
 	w graph.QuadWriter
 }
@@ -33,16 +41,25 @@ func (w quadWriter) WriteQuad(q quad.Quad) error {
 	return w.w.AddQuad(q)
 }
 
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	// Define an "ex:" prefix for IRIs that will be expanded to "http://example.org".
 	// "ex:name" will become "http://example.org/name"
 	voc.RegisterPrefix("ex:", "http://example.org/")
 
+	// Override a function to generate IDs. Can be changed to generate UUIDs, for example.
+	schema.GenerateID = func() quad.Value {
+		return quad.BNode(fmt.Sprintf("node%d", rand.Intn(1000)))
+	}
+
 	// File for your new BoltDB. Use path to regular file and not temporary in the real world
 	tmpfile, err := ioutil.TempFile("", "example")
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 
 	defer os.Remove(tmpfile.Name()) // clean up
 
@@ -51,37 +68,55 @@ func main() {
 
 	// Open and use the database
 	store, err := cayley.NewGraph("bolt", tmpfile.Name(), nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	checkErr(err)
 	defer store.Close()
 	qw := quadWriter{store.QuadWriter} // TODO: temporary workaround before formats PR merge
 
 	// Save an object
 	bob := Person{
-		ID:   quad.IRI("ex:bob"),
+		ID:   quad.IRI("ex:bob").Full().Short(),
 		Name: "Bob", Age: 32,
 	}
 	fmt.Printf("saving: %+v\n", bob)
 	id, err := schema.WriteAsQuads(qw, bob)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 	fmt.Println("id for object:", id, "=", bob.ID) // should be equal
 
 	// Get object by id
 	var someone Person
 	err = schema.SaveTo(nil, store, &someone, id)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 	fmt.Printf("loaded: %+v\n", someone)
 
 	// Or get all objects of type Person
 	var people []Person
 	err = schema.SaveTo(nil, store, &people)
-	if err != nil {
-		log.Fatal(err)
+	checkErr(err)
+	fmt.Printf("people: %+v\n", people)
+
+	fmt.Println()
+
+	// Store objects with no ID and type
+	coords := []Coords{
+		{Lat: 12.3, Lng: 34.5},
+		{Lat: 39.7, Lng: 8.41},
 	}
-	fmt.Printf("loaded slice: %+v\n", people)
+	for _, c := range coords {
+		id, err = schema.WriteAsQuads(qw, c)
+		checkErr(err)
+		fmt.Println("generated id:", id)
+	}
+
+	// Get coords back
+	var newCoords []Coords
+	err = schema.SaveTo(nil, store, &newCoords)
+	checkErr(err)
+	fmt.Printf("coords: %+v\n", newCoords)
+
+	// Print quads
+	fmt.Println("\nquads:")
+	it := store.QuadsAllIterator()
+	for it.Next() {
+		fmt.Println(store.Quad(it.Result()))
+	}
 }
