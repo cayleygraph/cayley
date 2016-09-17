0d2029f50daadbd0c1bab52b77244d1cd8728abd
Denys Smirnov <denis.smirnov.91@gmail.com>
2016-09-17 23:08:37 +0300

    add example for schema library

---
+++ examples/hello_schema/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/cayleygraph/cayley"
+	"github.com/cayleygraph/cayley/graph"
+	_ "github.com/cayleygraph/cayley/graph/bolt"
+	"github.com/cayleygraph/cayley/quad"
+	"github.com/cayleygraph/cayley/schema"
+	"github.com/cayleygraph/cayley/voc"
+	// Import RDF vocabulary definitions to be able to expand IRIs like rdf:label.
+	_ "github.com/cayleygraph/cayley/voc/core"
+)
+
+type Person struct {
+	// dummy field to enforce all object to have a <id> <rdf:type> <ex:Person> relation
+	// means nothing for Go itself
+	rdfType struct{} `quad:"@type > ex:Person"`
+	ID      quad.IRI `json:"@id"`     // tag @id is a special one - graph node value will be stored in this field
+	Name    string   `json:"ex:name"` // field name (predicate) may be written as json field name
+	Age     int      `quad:"ex:age"`  // or in a quad tag
+}
+
+// TODO: temporary workaround before formats PR merge
+type quadWriter struct {
+	w graph.QuadWriter
+}
+
+func (w quadWriter) WriteQuad(q quad.Quad) error {
+	return w.w.AddQuad(q)
+}
+
+func main() {
+	// Define an "ex:" prefix for IRIs that will be expanded to "http://example.org".
+	// "ex:name" will become "http://example.org/name"
+	voc.RegisterPrefix("ex:", "http://example.org/")
+
+	// File for your new BoltDB. Use path to regular file and not temporary in the real world
+	tmpfile, err := ioutil.TempFile("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	// Initialize the database
+	graph.InitQuadStore("bolt", tmpfile.Name(), nil)
+
+	// Open and use the database
+	store, err := cayley.NewGraph("bolt", tmpfile.Name(), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer store.Close()
+
+	// Save an object
+	bob := Person{
+		ID:   quad.IRI("ex:bob"),
+		Name: "Bob", Age: 32,
+	}
+	fmt.Printf("saving: %+v\n", bob)
+	id, err := schema.WriteAsQuads(quadWriter{store.QuadWriter}, bob)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("id for object:", id, "=", bob.ID) // should be equal
+
+	// Get object by id
+	var someone Person
+	err = schema.SaveTo(nil, store, &someone, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("loaded: %+v\n", someone)
+}
