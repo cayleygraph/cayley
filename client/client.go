9f3a4f70dac9f132bbfdf01d6be7c68c74067b1b
Denys Smirnov <denis.smirnov.91@gmail.com>
2017-04-23 10:40:19 +0300

    golang client for data import/export; add tests for v2 api

---
+++ client/client.go
@@ -0,0 +1,104 @@
+package client
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/cayleygraph/cayley/quad"
+	"github.com/cayleygraph/cayley/quad/pquads"
+)
+
+func New(addr string) *Client {
+	return &Client{addr: addr, cli: http.DefaultClient}
+}
+
+type Client struct {
+	addr string
+	cli  *http.Client
+}
+
+func (c *Client) SetHttpClient(cli *http.Client) {
+	c.cli = cli
+}
+func (c *Client) url(s string, q map[string]string) string {
+	addr := c.addr + s
+	if len(q) != 0 {
+		p := make(url.Values, len(q))
+		for k, v := range q {
+			p.Set(k, v)
+		}
+		addr += "?" + p.Encode()
+	}
+	return addr
+}
+
+type errRequestFailed struct {
+	Status     string
+	StatusCode int
+}
+
+func (e errRequestFailed) Error() string {
+	return fmt.Sprintf("request failed: %d %v", e.StatusCode, e.Status)
+}
+func (c *Client) QuadReader() (quad.ReadCloser, error) {
+	resp, err := http.Get(c.url("/api/v2/read", map[string]string{
+		"format": "pquads",
+	}))
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, errRequestFailed{StatusCode: resp.StatusCode, Status: resp.Status}
+	}
+	r := pquads.NewReader(resp.Body, 10*1024*1024)
+	r.SetCloser(resp.Body)
+	return r, nil
+}
+
+type funcCloser struct {
+	f      func() error
+	closed bool
+}
+
+func (c funcCloser) Close() error {
+	if c.closed {
+		return nil
+	}
+	c.closed = true
+	return c.f()
+}
+func (c *Client) QuadWriter() (quad.WriteCloser, error) {
+	pr, pw := io.Pipe()
+	req, err := http.NewRequest("POST", c.url("/api/v2/write", nil), pr)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", pquads.ContentType)
+	errc := make(chan error, 1)
+	go func() {
+		defer func() {
+			close(errc)
+			pr.Close()
+		}()
+		resp, err := c.cli.Do(req)
+		if resp != nil && resp.Body != nil {
+			defer resp.Body.Close()
+		}
+		if err == nil && resp.StatusCode != http.StatusOK {
+			err = errRequestFailed{StatusCode: resp.StatusCode, Status: resp.Status}
+		}
+		errc <- err
+	}()
+	qw := pquads.NewWriter(pw, &pquads.Options{
+		Full:   false,
+		Strict: false,
+	})
+	qw.SetCloser(funcCloser{f: func() error {
+		pw.Close()
+		return <-errc
+	}})
+	return qw, nil
+}
---
+++ server/http/api_v2_test.go
@@ -0,0 +1,66 @@
+package cayleyhttp
+
+import (
+	"net/http/httptest"
+	"sort"
+	"testing"
+
+	"github.com/cayleygraph/cayley/client"
+	"github.com/cayleygraph/cayley/graph"
+	"github.com/cayleygraph/cayley/graph/graphtest"
+	"github.com/cayleygraph/cayley/graph/memstore"
+	"github.com/cayleygraph/cayley/quad"
+	"github.com/cayleygraph/cayley/writer"
+	"github.com/stretchr/testify/require"
+)
+
+func makeHandle(t testing.TB, quads ...quad.Quad) *graph.Handle {
+	qs := memstore.New(quads...)
+	wr, err := writer.NewSingleReplication(qs, nil)
+	require.NoError(t, err)
+	return &graph.Handle{qs, wr}
+}
+
+func makeServerV2(t testing.TB, quads ...quad.Quad) (string, func()) {
+	h := makeHandle(t, quads...)
+
+	api2 := NewAPIv2(h)
+	srv := httptest.NewServer(api2)
+	addr := srv.Listener.Addr()
+	return "http://" + addr.String(), func() {
+		srv.Close()
+		h.Close()
+	}
+}
+
+func TestV2Write(t *testing.T) {
+	addr, closer := makeServerV2(t)
+	defer closer()
+
+	quads := graphtest.MakeQuadSet()
+	cli := client.New(addr)
+	qw, err := cli.QuadWriter()
+	require.NoError(t, err)
+	defer qw.Close()
+	n, err := quad.Copy(qw, quad.NewReader(quads))
+	require.NoError(t, err)
+	require.Equal(t, int(len(quads)), n)
+	err = qw.Close()
+	require.NoError(t, err)
+}
+
+func TestV2Read(t *testing.T) {
+	expect := graphtest.MakeQuadSet()
+	addr, closer := makeServerV2(t, expect...)
+	defer closer()
+
+	cli := client.New(addr)
+	qr, err := cli.QuadReader()
+	require.NoError(t, err)
+	defer qr.Close()
+	quads, err := quad.ReadAll(qr)
+	require.NoError(t, err)
+	sort.Sort(quad.ByQuadString(quads))
+	sort.Sort(quad.ByQuadString(expect))
+	require.Equal(t, expect, quads)
+}
