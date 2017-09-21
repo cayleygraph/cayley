c8bb680f9b69af9d35b025dc8cc16b19af686e6b
sah607 <Michael.Qiu@capitalone.com>
2017-09-21 13:59:58 -0700

    Add elastic as supported backend

--- cmd/cayley/cayley.go
+++ cmd/cayley/cayley.go
@@ -36,6 +36,7 @@ import (
 	// Load supported backends
 	_ "github.com/cayleygraph/cayley/graph/bolt"
 	_ "github.com/cayleygraph/cayley/graph/bolt2"
+	_ "github.com/cayleygraph/cayley/graph/elastic"
 	_ "github.com/cayleygraph/cayley/graph/leveldb"
 	_ "github.com/cayleygraph/cayley/graph/memstore"
 	_ "github.com/cayleygraph/cayley/graph/mongo"
