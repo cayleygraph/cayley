8db73d70044221de0f722fa1ec9ccd9756268d98
camazotz <navalava@gmail.com>
2017-12-14 14:24:24 -0800

    Comment optimizer code

--- graph/bolt/quadstore_iterator_optimize.go
+++ graph/bolt/quadstore_iterator_optimize.go
@@ -29,27 +29,27 @@ func (qs *QuadStore) OptimizeIterator(it graph.Iterator) (graph.Iterator, bool) 
 }
 
 func (qs *QuadStore) optimizeLinksTo(it *iterator.LinksTo) (graph.Iterator, bool) {
-	subs := it.SubIterators()
-	if len(subs) != 1 {
-		return it, false
-	}
-	primary := subs[0]
-	if primary.Type() == graph.Fixed {
-		size, _ := primary.Size()
-		if size == 1 {
-			if !primary.Next() {
-				panic("unexpected size during optimize")
-			}
-			val := primary.Result()
-			newIt := qs.QuadIterator(it.Direction(), val)
-			nt := newIt.Tagger()
-			nt.CopyFrom(it)
-			for _, tag := range primary.Tagger().Tags() {
-				nt.AddFixed(tag, val)
-			}
-			it.Close()
-			return newIt, true
-		}
-	}
+	// subs := it.SubIterators()
+	// if len(subs) != 1 {
+	// 	return it, false
+	// }
+	// primary := subs[0]
+	// if primary.Type() == graph.Fixed {
+	// 	size, _ := primary.Size()
+	// 	if size == 1 {
+	// 		if !primary.Next() {
+	// 			panic("unexpected size during optimize")
+	// 		}
+	// 		val := primary.Result()
+	// 		newIt := qs.QuadIterator(it.Direction(), val)
+	// 		nt := newIt.Tagger()
+	// 		nt.CopyFrom(it)
+	// 		for _, tag := range primary.Tagger().Tags() {
+	// 			nt.AddFixed(tag, val)
+	// 		}
+	// 		it.Close()
+	// 		return newIt, true
+	// 	}
+	// }
 	return it, false
 }
