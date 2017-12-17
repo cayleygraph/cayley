7164e734cc6f73b729ca489da77648fd53fabb73
Denys Smirnov <denis.smirnov.91@gmail.com>
2017-12-17 17:24:52 +0200

    mongo: import legacy version as mongo1

--- graph/all/all.go
+++ graph/all/all.go
@@ -1,11 +1,15 @@
 package all
 
 import (
+	// legacy backends (will be deprecated)
 	_ "github.com/cayleygraph/cayley/graph/bolt"
+	_ "github.com/cayleygraph/cayley/graph/leveldb"
+	_ "github.com/cayleygraph/cayley/graph/mongo"
+
+	// supported backends
 	_ "github.com/cayleygraph/cayley/graph/kv/bolt"
 	_ "github.com/cayleygraph/cayley/graph/kv/btree"
 	_ "github.com/cayleygraph/cayley/graph/kv/leveldb"
-	_ "github.com/cayleygraph/cayley/graph/leveldb"
 	_ "github.com/cayleygraph/cayley/graph/memstore"
 	_ "github.com/cayleygraph/cayley/graph/nosql/elastic"
 	_ "github.com/cayleygraph/cayley/graph/nosql/mongo"
