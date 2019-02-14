099b94169b85356dbea0bfbf1ca3160b64d98e9f
Luis Fung <fungl164@gmail.com>
2019-02-14 15:46:26 -0500

    Enable native byte array fields on structs

--- voc/schema/schema.go
+++ voc/schema/schema.go
@@ -25,6 +25,8 @@ const (
 	True = Prefix + `True`
 	// Data type: Text.
 	Text = Prefix + `Text`
+	// Data type: ByteArr.
+	ByteArr = Prefix + "ByteArr"
 	// Data type: URL.
 	URL = Prefix + `URL`
 	// Data type: Number.
