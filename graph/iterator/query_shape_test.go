be92b16a56023e47ffe4551c892da802a47dd846
Denys Smirnov <denis.smirnov.91@gmail.com>
2019-07-28 04:21:25 +0300

    iterator: allow re-wrapping the new iterators back and forth

--- graph/iterator/query_shape_test.go
+++ graph/iterator/query_shape_test.go
@@ -53,7 +53,7 @@ func TestQueryShape(t *testing.T) {
 
 	nodes := shape["nodes"].([]Node)
 	if len(nodes) != 3 {
-		t.Fatalf("Failed to get correct number of nodes, got:%d expect:4", len(nodes))
+		t.Fatalf("Failed to get correct number of nodes, got:%d expect:3", len(nodes))
 	}
 	links := shape["links"].([]Link)
 	if len(nodes) != 3 {
