466dcefddbce461c2d764766a229cae7c454f5f5
Denys Smirnov <denis.smirnov.91@gmail.com>
2019-08-18 10:11:54 +0300

    sql: rewrite iterators for the new interfaces

--- graph/sql/iterator.go
+++ graph/sql/iterator.go
@@ -56,39 +56,126 @@ func (qs *QuadStore) Query(ctx context.Context, s Shape) (*sql.Rows, error) {
 	return rows, nil
 }
 
-var _ graph.Iterator = (*Iterator)(nil)
+var _ graph.IteratorFuture = (*Iterator)(nil)
 
 func (qs *QuadStore) NewIterator(s Select) *Iterator {
-	return &Iterator{
+	it := &Iterator{
+		it: qs.newIterator(s),
+	}
+	it.Iterator = graph.NewLegacy(it.it, it)
+	return it
+}
+
+type Iterator struct {
+	it *iterator2
+	graph.Iterator
+}
+
+func (it *Iterator) AsShape() graph.Shape {
+	it.Close()
+	return it.it
+}
+
+var _ graph.ShapeCompat = (*iterator2)(nil)
+
+func (qs *QuadStore) newIterator(s Select) *iterator2 {
+	return &iterator2{
 		qs:    qs,
 		query: s,
 	}
 }
 
-type Iterator struct {
+type iterator2 struct {
+	qs    *QuadStore
+	query Select
+	err   error
+}
+
+func (it *iterator2) Iterate() graph.Scanner {
+	return newIteratorNext(it.qs, it.query)
+}
+
+func (it *iterator2) Lookup() graph.Index {
+	return newIteratorContains(it.qs, it.query)
+}
+
+func (it *iterator2) AsLegacy() graph.Iterator {
+	it2 := &Iterator{it: it}
+	it2.Iterator = graph.NewLegacy(it, it2)
+	return it2
+}
+
+func (it *iterator2) Stats(ctx context.Context) (graph.IteratorCosts, error) {
+	sz, err := it.getSize(ctx)
+	return graph.IteratorCosts{
+		NextCost:     1,
+		ContainsCost: 10,
+		Size:         sz,
+	}, err
+}
+
+func (it *iterator2) estimateSize(ctx context.Context) int64 {
+	if it.query.Limit > 0 {
+		return it.query.Limit
+	}
+	st, err := it.qs.Stats(ctx, false)
+	if err != nil && it.err == nil {
+		it.err = err
+	}
+	return st.Quads.Size
+}
+
+func (it *iterator2) getSize(ctx context.Context) (graph.Size, error) {
+	sz, err := it.qs.querySize(ctx, it.query)
+	if err != nil {
+		it.err = err
+		return graph.Size{Size: it.estimateSize(ctx), Exact: false}, err
+	}
+	return sz, nil
+}
+
+func (it *iterator2) Optimize(ctx context.Context) (graph.Shape, bool) {
+	return it, false
+}
+
+func (it *iterator2) SubIterators() []graph.Shape {
+	return nil
+}
+
+func (it *iterator2) String() string {
+	return it.query.SQL(NewBuilder(it.qs.flavor.QueryDialect))
+}
+
+func newIteratorBase(qs *QuadStore, s Select) iteratorBase {
+	return iteratorBase{
+		qs:    qs,
+		query: s,
+	}
+}
+
+type iteratorBase struct {
 	qs    *QuadStore
 	query Select
 
 	cols []string
 	cind map[quad.Direction]int
 
-	err    error
-	res    graph.Ref
-	tags   map[string]graph.Ref
-	cursor *sql.Rows
+	err  error
+	res  graph.Ref
+	tags map[string]graph.Ref
 }
 
-func (it *Iterator) TagResults(m map[string]graph.Ref) {
+func (it *iteratorBase) TagResults(m map[string]graph.Ref) {
 	for tag, val := range it.tags {
 		m[tag] = val
 	}
 }
 
-func (it *Iterator) Result() graph.Ref {
+func (it *iteratorBase) Result() graph.Ref {
 	return it.res
 }
 
-func (it *Iterator) ensureColumns() {
+func (it *iteratorBase) ensureColumns() {
 	if it.cols != nil {
 		return
 	}
@@ -112,7 +199,7 @@ func (it *Iterator) ensureColumns() {
 	}
 }
 
-func (it *Iterator) scanValue(r *sql.Rows) bool {
+func (it *iteratorBase) scanValue(r *sql.Rows) bool {
 	it.ensureColumns()
 	nodes := make([]NodeHash, len(it.cols))
 	pointers := make([]interface{}, len(nodes))
@@ -151,7 +238,99 @@ func (it *Iterator) scanValue(r *sql.Rows) bool {
 	return true
 }
 
-func (it *Iterator) Next(ctx context.Context) bool {
+func (it *iteratorBase) Err() error {
+	return it.err
+}
+
+func (it *iteratorBase) String() string {
+	return it.query.SQL(NewBuilder(it.qs.flavor.QueryDialect))
+}
+
+func newIteratorNext(qs *QuadStore, s Select) *iteratorNext {
+	return &iteratorNext{
+		iteratorBase: newIteratorBase(qs, s),
+	}
+}
+
+type iteratorNext struct {
+	iteratorBase
+	cursor *sql.Rows
+}
+
+func (it *iteratorNext) TagResults(m map[string]graph.Ref) {
+	for tag, val := range it.tags {
+		m[tag] = val
+	}
+}
+
+func (it *iteratorNext) Result() graph.Ref {
+	return it.res
+}
+
+func (it *iteratorNext) ensureColumns() {
+	if it.cols != nil {
+		return
+	}
+	it.cols = it.query.Columns()
+	it.cind = make(map[quad.Direction]int, len(quad.Directions)+1)
+	for i, name := range it.cols {
+		if !strings.HasPrefix(name, tagPref) {
+			continue
+		}
+		if name == tagNode {
+			it.cind[quad.Any] = i
+			continue
+		}
+		name = name[len(tagPref):]
+		for _, d := range quad.Directions {
+			if name == d.String() {
+				it.cind[d] = i
+				break
+			}
+		}
+	}
+}
+
+func (it *iteratorNext) scanValue(r *sql.Rows) bool {
+	it.ensureColumns()
+	nodes := make([]NodeHash, len(it.cols))
+	pointers := make([]interface{}, len(nodes))
+	for i := range pointers {
+		pointers[i] = &nodes[i]
+	}
+	if err := r.Scan(pointers...); err != nil {
+		it.err = err
+		return false
+	}
+	it.tags = make(map[string]graph.Ref)
+	for i, name := range it.cols {
+		if !strings.Contains(name, tagPref) {
+			it.tags[name] = nodes[i].ValueHash
+		}
+	}
+	if len(it.cind) > 1 {
+		var q QuadHashes
+		for _, d := range quad.Directions {
+			i, ok := it.cind[d]
+			if !ok {
+				it.err = fmt.Errorf("cannot find quad %v in query output (columns: %v)", d, it.cols)
+				return false
+			}
+			q.Set(d, nodes[i].ValueHash)
+		}
+		it.res = q
+		return true
+	}
+	i, ok := it.cind[quad.Any]
+	if !ok {
+		it.err = fmt.Errorf("cannot find node hash in query output (columns: %v, cind: %v)", it.cols, it.cind)
+		return false
+	}
+	it.res = nodes[i]
+	return true
+}
+
+func (it *iteratorNext) Next(ctx context.Context) bool {
 	if it.err != nil {
 		return false
 	}
@@ -169,11 +348,114 @@ func (it *Iterator) Next(ctx context.Context) bool {
 	return it.scanValue(it.cursor)
 }
 
-func (it *Iterator) NextPath(ctx context.Context) bool {
+func (it *iteratorNext) NextPath(ctx context.Context) bool {
 	return false
 }
 
-func (it *Iterator) Contains(ctx context.Context, v graph.Ref) bool {
+func (it *iteratorNext) Err() error {
+	return it.err
+}
+
+func (it *iteratorNext) String() string {
+	return it.query.SQL(NewBuilder(it.qs.flavor.QueryDialect))
+}
+
+func (it *iteratorNext) Close() error {
+	if it.cursor != nil {
+		it.cursor.Close()
+		it.cursor = nil
+	}
+	return nil
+}
+
+func newIteratorContains(qs *QuadStore, s Select) *iteratorContains {
+	return &iteratorContains{
+		iteratorBase: newIteratorBase(qs, s),
+	}
+}
+
+type iteratorContains struct {
+	iteratorBase
+}
+
+func (it *iteratorContains) TagResults(m map[string]graph.Ref) {
+	for tag, val := range it.tags {
+		m[tag] = val
+	}
+}
+
+func (it *iteratorContains) Result() graph.Ref {
+	return it.res
+}
+
+func (it *iteratorContains) ensureColumns() {
+	if it.cols != nil {
+		return
+	}
+	it.cols = it.query.Columns()
+	it.cind = make(map[quad.Direction]int, len(quad.Directions)+1)
+	for i, name := range it.cols {
+		if !strings.HasPrefix(name, tagPref) {
+			continue
+		}
+		if name == tagNode {
+			it.cind[quad.Any] = i
+			continue
+		}
+		name = name[len(tagPref):]
+		for _, d := range quad.Directions {
+			if name == d.String() {
+				it.cind[d] = i
+				break
+			}
+		}
+	}
+}
+
+func (it *iteratorContains) scanValue(r *sql.Rows) bool {
+	it.ensureColumns()
+	nodes := make([]NodeHash, len(it.cols))
+	pointers := make([]interface{}, len(nodes))
+	for i := range pointers {
+		pointers[i] = &nodes[i]
+	}
+	if err := r.Scan(pointers...); err != nil {
+		it.err = err
+		return false
+	}
+	it.tags = make(map[string]graph.Ref)
+	for i, name := range it.cols {
+		if !strings.Contains(name, tagPref) {
+			it.tags[name] = nodes[i].ValueHash
+		}
+	}
+	if len(it.cind) > 1 {
+		var q QuadHashes
+		for _, d := range quad.Directions {
+			i, ok := it.cind[d]
+			if !ok {
+				it.err = fmt.Errorf("cannot find quad %v in query output (columns: %v)", d, it.cols)
+				return false
+			}
+			q.Set(d, nodes[i].ValueHash)
+		}
+		it.res = q
+		return true
+	}
+	i, ok := it.cind[quad.Any]
+	if !ok {
+		it.err = fmt.Errorf("cannot find node hash in query output (columns: %v, cind: %v)", it.cols, it.cind)
+		return false
+	}
+	it.res = nodes[i]
+	return true
+}
+
+func (it *iteratorContains) NextPath(ctx context.Context) bool {
+	return false
+}
+
+func (it *iteratorContains) Contains(ctx context.Context, v graph.Ref) bool {
 	it.ensureColumns()
 	sel := it.query
 	sel.Where = append([]Where{}, sel.Where...)
@@ -215,66 +497,14 @@ func (it *Iterator) Contains(ctx context.Context, v graph.Ref) bool {
 	return it.scanValue(rows)
 }
 
-func (it *Iterator) Err() error {
+func (it *iteratorContains) Err() error {
 	return it.err
 }
 
-func (it *Iterator) Reset() {
-	it.cols = nil
-	it.cind = nil
-	it.res = nil
-	it.err = nil
-	if it.cursor != nil {
-		it.cursor.Close()
-		it.cursor = nil
-	}
-}
-
-func (it *Iterator) Stats() graph.IteratorStats {
-	sz, exact := it.Size()
-	return graph.IteratorStats{
-		NextCost:     1,
-		ContainsCost: 10,
-		Size:         sz, ExactSize: exact,
-	}
-}
-
-func (it *Iterator) estimateSize() int64 {
-	if it.query.Limit > 0 {
-		return it.query.Limit
-	}
-	st, err := it.qs.Stats(context.TODO(), false)
-	if err != nil && it.err == nil {
-		it.err = err
-	}
-	return st.Quads.Size
-}
-
-func (it *Iterator) Size() (int64, bool) {
-	sz, err := it.qs.querySize(context.TODO(), it.query)
-	if err != nil {
-		it.err = err
-		return it.estimateSize(), false
-	}
-	return sz.Size, sz.Exact
-}
-
-func (it *Iterator) Optimize() (graph.Iterator, bool) {
-	return it, false
-}
-
-func (it *Iterator) SubIterators() []graph.Iterator {
-	return nil
-}
-
-func (it *Iterator) String() string {
+func (it *iteratorContains) String() string {
 	return it.query.SQL(NewBuilder(it.qs.flavor.QueryDialect))
 }
 
-func (it *Iterator) Close() error {
-	if it.cursor != nil {
-		it.cursor.Close()
-		it.cursor = nil
-	}
+func (it *iteratorContains) Close() error {
 	return nil
 }
