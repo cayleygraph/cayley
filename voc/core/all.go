4ca750092bc32530f85ca0af32b30a62d3491ae5
Denys Smirnov <denis.smirnov.91@gmail.com>
2019-10-15 11:45:01 +0300

    remove deprecated voc package

--- voc/core/all.go
+++
@@ -1,8 +0,0 @@
-// Package core is deprecated. Use github.com/cayleygraph/quad/voc/core.
-package core
-
-import (
-	_ "github.com/cayleygraph/quad/voc/rdf"
-	_ "github.com/cayleygraph/quad/voc/rdfs"
-	_ "github.com/cayleygraph/quad/voc/schema"
-)
