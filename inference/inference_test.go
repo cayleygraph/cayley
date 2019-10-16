75e74ca3980277d66993d14603664c4bed469866
Iddan Aaronsohn <mail@aniddan.com>
2019-10-16 12:54:37 +0300

    Test creations

--- inference/inference_test.go
+++ inference/inference_test.go
@@ -5,14 +5,35 @@ import (
 
 	"github.com/cayleygraph/quad"
 	"github.com/cayleygraph/quad/voc/rdf"
+	"github.com/cayleygraph/quad/voc/rdfs"
 )
 
-func TestStoreProcessQuad(t *testing.T) {
+func TestReferencedType(t *testing.T) {
 	store := NewStore()
 	q := quad.Quad{Subject: quad.IRI("alice"), Predicate: quad.IRI(rdf.Type), Object: quad.IRI("Person"), Label: nil}
 	store.ProcessQuad(q)
 	createdClass := store.GetClass(quad.IRI("Person"))
 	if createdClass == nil {
 		t.Error("Class was not created")
+	}
+}
+
+func TestNewClass(t *testing.T) {
+	store := NewStore()
+	q := quad.Quad{Subject: quad.IRI("Person"), Predicate: quad.IRI(rdf.Type), Object: quad.IRI(rdfs.Class), Label: nil}
+	store.ProcessQuad(q)
+	createdClass := store.GetClass(quad.IRI("Person"))
+	if createdClass == nil {
+		t.Error("Class was not created")
+	}
+}
+
+func TestNewProperty(t *testing.T) {
+	store := NewStore()
+	q := quad.Quad{Subject: quad.IRI("name"), Predicate: quad.IRI(rdf.Type), Object: quad.IRI(rdf.Property), Label: nil}
+	store.ProcessQuad(q)
+	createdProperty := store.GetProperty(quad.IRI("name"))
+	if createdProperty == nil {
+		t.Error("Property was not created")
 	}
 }
