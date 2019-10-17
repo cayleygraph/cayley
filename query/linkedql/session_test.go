d7d11d4222d9ecbbf4f48904e445af0d2c849a19
Iddan Aaronsohn <mail@aniddan.com>
2019-10-17 16:04:59 +0300

    linkedql: Better description of Vertex, Description of Out

--- query/linkedql/session_test.go
+++ query/linkedql/session_test.go
@@ -11,7 +11,35 @@ import (
 func TestNodeQuery(t *testing.T) {
 	q := `
 {
-	"@type": "linkedql:NewVertex"
+	"@type": "linkedql:Vertex"
+}
+	`
+	store := memstore.New()
+	session := NewSession(store)
+	iterator, err := session.Execute(context.TODO(), q, query.Options{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if iterator.Result() != nil {
+		t.Error("Returned result for an empty store")
+	}
+}
+
+func TestOutQuery(t *testing.T) {
+	q := `
+{
+	"@type": "linkedql:Out",
+	"from": {
+		"@type": "linkedql:Vertex"
+	},
+	"via": {
+		"@type": "linkedql:Vertex",
+		"values": [
+			{
+				"@id": "likes"
+			}
+		]
+	}
 }
 	`
 	store := memstore.New()
