42da117f2b8c1ebe4810dd5e4e2af784c97a9aae
Iddan Aaronsohn <mail@aniddan.com>
2019-10-17 14:48:22 +0300

    Create package linkedql Register linkedql as a query language

---
+++ query/linkedql/linkedql.go
@@ -0,0 +1,21 @@
+package linkedql
+
+import (
+	"github.com/cayleygraph/cayley/graph"
+	"github.com/cayleygraph/cayley/query"
+)
+
+// Name is the name exposed to the query interface
+const Name = "linkedql"
+
+func init() {
+	query.RegisterLanguage(query.Language{
+		Name: Name,
+		Session: func(qs graph.QuadStore) query.Session {
+			return NewSession(qs)
+		},
+		HTTP: func(qs graph.QuadStore) query.HTTP {
+			return NewSession(qs)
+		},
+	})
+}
