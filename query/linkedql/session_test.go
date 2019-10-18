c18830a5bf895516fcb0ae56bd97929b1cb92069
Iddan Aaronsohn <mail@aniddan.com>
2019-10-18 14:47:33 +0300

    linkedql: Test Back, Both, Count, Except

--- query/linkedql/session_test.go
+++ query/linkedql/session_test.go
@@ -168,6 +168,102 @@ var testCases = []struct {
 			quad.IRI("bob"),
 		},
 	},
+	{
+		name: "Back",
+		data: []quad.Quad{
+			quad.MakeIRI("alice", "likes", "bob", ""),
+		},
+		query: `
+		{
+			"@type": "linkedql:Back",
+			"from": {
+				"@type": "linkedql:Out",
+				"from": {
+					"@type": "linkedql:Vertex",
+					"values": [{ "@id": "alice" }]
+				},
+				"via": {
+					"@type": "linkedql:Vertex",
+					"values": [
+						{
+							"@id": "likes"
+						}
+					]
+				}
+			}
+		}
+		`,
+		results: []interface{}{
+			quad.IRI("alice"),
+		},
+	},
+	{
+		name: "Both",
+		data: []quad.Quad{
+			quad.MakeIRI("alice", "likes", "bob", ""),
+			quad.MakeIRI("bob", "likes", "dan", ""),
+		},
+		query: `
+		{
+			"@type": "linkedql:Both",
+			"from": {
+				"@type": "linkedql:Vertex",
+				"values": [{ "@id": "bob" }]
+			},
+			"via": {
+				"@type": "linkedql:Vertex",
+				"values": [
+					{
+						"@id": "likes"
+					}
+				]
+			}
+		}
+		`,
+		results: []interface{}{
+			quad.IRI("alice"),
+			quad.IRI("dan"),
+		},
+	},
+	{
+		name: "Count",
+		data: []quad.Quad{
+			quad.MakeIRI("alice", "likes", "bob", ""),
+		},
+		query: `
+		{
+			"@type": "linkedql:Count",
+			"from": {
+				"@type": "linkedql:Vertex"
+			}
+		}
+		`,
+		results: []interface{}{
+			quad.Int(4),
+		},
+	},
+	{
+		name: "Except",
+		data: []quad.Quad{
+			quad.MakeIRI("alice", "likes", "bob", ""),
+		},
+		query: `
+		{
+			"@type": "linkedql:Except",
+			"from": {
+				"@type": "linkedql:Vertex",
+				"values": [{ "@id": "alice" }, { "@id": "likes" }]
+			},
+			"excepted": {
+				"@type": "linkedql:Vertex",
+				"values": [{ "@id": "likes" }]
+			}
+		}
+		`,
+		results: []interface{}{
+			quad.IRI("alice"),
+		},
+	},
 }
 
 func TestLinkedQL(t *testing.T) {
