e2ec75fa7a37ce840e73c160c6bdf850dd93d064
Iddan Aaronsohn <mail@aniddan.com>
2019-10-23 00:14:55 +0200

    linkedql: Add comments to value_iterator.go

--- query/linkedql/value_iterator.go
+++ query/linkedql/value_iterator.go
@@ -7,16 +7,19 @@ import (
 	"github.com/cayleygraph/cayley/query/path"
 )
 
+// ValueIterator is an iterator of values from the graph
 type ValueIterator struct {
 	namer   graph.Namer
 	path    *path.Path
 	scanner graph.Scanner
 }
 
+// NewValueIterator returns a new ValueIterator for a path and namer
 func NewValueIterator(p *path.Path, namer graph.Namer) *ValueIterator {
 	return &ValueIterator{namer, p, nil}
 }
 
+// Next implements query.Iterator
 func (it *ValueIterator) Next(ctx context.Context) bool {
 	if it.scanner == nil {
 		it.scanner = it.path.BuildIterator().Iterate()
@@ -24,6 +27,7 @@ func (it *ValueIterator) Next(ctx context.Context) bool {
 	return it.scanner.Next(ctx)
 }
 
+// Result implements query.Iterator
 func (it *ValueIterator) Result() interface{} {
 	if it.scanner == nil {
 		return nil
@@ -31,6 +35,7 @@ func (it *ValueIterator) Result() interface{} {
 	return it.namer.NameOf(it.scanner.Result())
 }
 
+// Err implements query.Iterator
 func (it *ValueIterator) Err() error {
 	if it.scanner == nil {
 		return nil
@@ -38,6 +43,7 @@ func (it *ValueIterator) Err() error {
 	return it.scanner.Err()
 }
 
+// Close implements query.Iterator
 func (it *ValueIterator) Close() error {
 	if it.scanner == nil {
 		return nil
