9defb653e571e7906885209f02c509019ce255d8
Iddan Aaronsohn <mail@aniddan.com>
2019-11-10 23:38:32 +0200

    linkedql: Refactor out ValueStep for PathStep only

--- query/linkedql/final_steps.go
+++ query/linkedql/final_steps.go
@@ -2,7 +2,9 @@ package linkedql
 
 import (
 	"github.com/cayleygraph/cayley/graph"
+	"github.com/cayleygraph/cayley/graph/refs"
 	"github.com/cayleygraph/cayley/query"
+	"github.com/cayleygraph/cayley/query/path"
 	"github.com/cayleygraph/quad"
 )
 
@@ -14,8 +16,8 @@ func init() {
 
 // Select corresponds to .select().
 type Select struct {
-	Tags []string  `json:"tags"`
-	From ValueStep `json:"from"`
+	Tags []string `json:"tags"`
+	From PathStep `json:"from"`
 }
 
 // Type implements Step.
@@ -25,17 +27,17 @@ func (s *Select) Type() quad.IRI {
 
 // BuildIterator implements Step.
 func (s *Select) BuildIterator(qs graph.QuadStore) (query.Iterator, error) {
-	fromIt, err := s.From.BuildValueIterator(qs)
+	p, err := s.From.BuildPath(qs)
 	if err != nil {
 		return nil, err
 	}
-	return &TagsIterator{fromIt, s.Tags}, nil
+	return &TagsIterator{valueIt: NewValueIterator(p, qs), selected: s.Tags}, nil
 }
 
 // SelectFirst corresponds to .selectFirst().
 type SelectFirst struct {
-	Tags []string  `json:"tags"`
-	From ValueStep `json:"from"`
+	Tags []string `json:"tags"`
+	From PathStep `json:"from"`
 }
 
 // Type implements Step.
@@ -43,23 +45,22 @@ func (s *SelectFirst) Type() quad.IRI {
 	return prefix + "SelectFirst"
 }
 
-func singleValueIterator(it *ValueIterator) *ValueIterator {
-	p := it.path.Limit(1)
-	return NewValueIterator(p, it.namer)
+func singleValueIterator(p *path.Path, namer refs.Namer) *ValueIterator {
+	return NewValueIterator(p.Limit(1), namer)
 }
 
 // BuildIterator implements Step.
 func (s *SelectFirst) BuildIterator(qs graph.QuadStore) (query.Iterator, error) {
-	it, err := s.From.BuildValueIterator(qs)
+	p, err := s.From.BuildPath(qs)
 	if err != nil {
 		return nil, err
 	}
-	return &TagsIterator{singleValueIterator(it), s.Tags}, nil
+	return &TagsIterator{singleValueIterator(p, qs), s.Tags}, nil
 }
 
 // Value corresponds to .value().
 type Value struct {
-	From ValueStep `json:"from"`
+	From PathStep `json:"from"`
 }
 
 // Type implements Step.
@@ -69,11 +70,11 @@ func (s *Value) Type() quad.IRI {
 
 // BuildIterator implements Step.
 func (s *Value) BuildIterator(qs graph.QuadStore) (query.Iterator, error) {
-	it, err := s.From.BuildValueIterator(qs)
+	p, err := s.From.BuildPath(qs)
 	if err != nil {
 		return nil, err
 	}
-	return singleValueIterator(it), nil
+	return singleValueIterator(p, qs), nil
 }
 
 // Documents corresponds to .documents().
