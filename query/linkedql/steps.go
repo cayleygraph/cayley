3c77c62d6f243aa747e5f4b5d48268b43ee053b4
Iddan Aaronsohn <mail@aniddan.com>
2019-12-01 01:24:34 +0200

    linkedql: Add missing prefix to Placeholder type

--- query/linkedql/steps.go
+++ query/linkedql/steps.go
@@ -233,7 +233,7 @@ type Placeholder struct{}
 
 // Type implements Step.
 func (s *Placeholder) Type() quad.IRI {
-	return "Placeholder"
+	return Prefix + "Placeholder"
 }
 
 // Description implements Step.
