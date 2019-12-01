13e3d546190f5175938d08dc6bca0e13bad1d969
Iddan Aaronsohn <mail@aniddan.com>
2019-12-01 23:17:07 +0200

    Defined better API and loaded json schema to quad store

--- query/linkedql/client/client2.go
+++ query/linkedql/client/client2.go
@@ -2,13 +2,56 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"go/ast"
 	"go/format"
 	"go/token"
+	"io"
+	"os"
+
+	"github.com/cayleygraph/cayley/graph"
+	"github.com/cayleygraph/cayley/graph/memstore"
+	"github.com/cayleygraph/cayley/owl"
+	"github.com/cayleygraph/quad"
+	"github.com/cayleygraph/quad/jsonld"
 )
 
+const schemaFile = "linkedql.json"
+
+func loadSchema() (graph.QuadStore, error) {
+	jsonFile, err := os.Open(schemaFile)
+	if err != nil {
+		return nil, err
+	}
+	var o interface{}
+	qs := memstore.New()
+	json.NewDecoder(jsonFile).Decode(&o)
+	reader := jsonld.NewReaderFromMap(o)
+	for true {
+		quad, err := reader.ReadQuad()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		qs.AddQuad(quad)
+	}
+	return qs, nil
+}
+
 func main() {
+	qs, err := loadSchema()
+
+	stepClass := owl.GetClass(qs, quad.IRI("linkedql:Step"))
+	stepSubClasses := stepClass.SubClasses()
+
+	fmt.Printf("%v\n", stepSubClasses)
+
+	if err != nil {
+		panic(err)
+	}
+
 	file := &ast.File{
 		Name: ast.NewIdent("example"),
 		Decls: []ast.Decl{
@@ -26,7 +69,7 @@ func main() {
 	fset := token.NewFileSet()
 
 	var buf bytes.Buffer
-	err := format.Node(&buf, fset, file)
+	err = format.Node(&buf, fset, file)
 	if err != nil {
 		panic(err)
 	}
