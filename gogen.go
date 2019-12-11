8292cd0b294a08fdb5d4ed73e46bf9721d75955f
Iddan Aaronsohn <mail@aniddan.com>
2019-12-12 00:10:30 +0200

    Make client importable

--- gogen.go
+++ gogen.go
@@ -1,3 +1,4 @@
 package cayley
 
-//go:generate docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/api/swagger.yml -g go -o /local/client
+//go:generate docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/api/swagger.yml -g go -o /local/client -c /local/client/config.json
+//go:generate rm client/go.sum client/go.mod
