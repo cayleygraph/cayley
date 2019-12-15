f4d927f8eacf1db9bf1ccdedb9f074bd956e96bc
Iddan Aaronsohn <mail@aniddan.com>
2019-12-15 22:07:34 +0200

    Revert all non-server changes

--- gogen.go
+++ gogen.go
@@ -1,4 +1,3 @@
 package cayley
 
-//go:generate docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/api/swagger.yml -g go -o /local/client -c /local/client/config.json
-//go:generate rm client/go.sum client/go.mod
+//go:generate go run ./cmd/docgen/docgen.go -i ./docs/GizmoAPI.md.in -o ./docs/GizmoAPI.md
