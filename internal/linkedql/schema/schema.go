89d3a4d566efda5da03a28e1b97d578c36febdc2
Iddan Aaronsohn <mail@aniddan.com>
2019-12-20 20:49:28 +0200

    linkedql: Correct form of @context

--- internal/linkedql/schema/schema.go
+++ internal/linkedql/schema/schema.go
@@ -269,11 +269,11 @@ func (g *generator) Generate() []byte {
 	graph = append(graph, g.out...)
 	data, err := json.Marshal(map[string]interface{}{
 		"@context": map[string]interface{}{
-			"rdf":      identified{ID: "http://www.w3.org/1999/02/22-rdf-syntax-ns#"},
-			"rdfs":     identified{ID: "http://www.w3.org/2000/01/rdf-schema#"},
-			"owl":      identified{ID: "http://www.w3.org/2002/07/owl#"},
-			"xsd":      identified{ID: "http://www.w3.org/2001/XMLSchema#"},
-			"linkedql": identified{ID: "http://cayley.io/linkedql#"},
+			"rdf":      "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+			"rdfs":     "http://www.w3.org/2000/01/rdf-schema#",
+			"owl":      "http://www.w3.org/2002/07/owl#",
+			"xsd":      "http://www.w3.org/2001/XMLSchema#",
+			"linkedql": "http://cayley.io/linkedql#",
 		},
 		"@graph": graph,
 	})
